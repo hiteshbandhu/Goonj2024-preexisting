63b03f34d3e73167d96fc5555fced2b7bb1b6b8c
uav <uav0108@gmail.com>
2023-02-11 18:40:28 +0530

    replaced main logo and improved responsiveness

--- src/pages/sponsers.tsx
+++ src/pages/sponsers.tsx
@@ -26,7 +26,7 @@ export default function Sponsers(){
            />
            <div className={styles.tplayer}></div>
            <div className={styles.sps}>
-            <a href="/Marketing Brochure - GOONJ 2023.pdf" download>Download Marketing Brochure</a>
+            <a href="https://bit.ly/3Yujgba" download>Download Marketing Brochure</a>
            </div>
            <div className={styles.sponsors}>
             <h2>Title Sponsor</h2>
