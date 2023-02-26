20dab09450dc3664be9db527e9573354364c8fd6
uav <uav0108@gmail.com>
2023-02-26 12:57:31 +0530

    added animation

--- src/components/marque/marque.tsx
+++ src/components/marque/marque.tsx
@@ -1,23 +1,24 @@
 import Image from "next/image"
 import styles from "./marque.module.scss"
 export default function Marque(){
+    const size =80;
     return (
         <div className={styles.container}>
-            <marquee scrollAmount={8} scrollDelay={60}>
+            <div className={styles.move}>
 
             <Image
             src="/sponsors/Untitled design (13).png"
             alt="decathon"
-            width={100}
-            height={100}
+            width={size}
+            height={size}
             />
             <Image
             src="/sponsors/Untitled design (14).png"
             alt="decathon"
-            width={100}
-            height={100}
+            width={size}
+            height={size}
             />
-            </marquee>
+            </div>
         </div>
     )
 }
